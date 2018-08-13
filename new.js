5097ebefa59b8eae5fe11e10c8412b9d154f3313
lex <kasatkin-11@yandex.ru>
2018-08-14 00:14:27 +0700

    changes in styles.css

--- new.js
+++ new.js
@@ -1,3 +1,11 @@
 "use strict";
 console.log(1 + 3);
 console.log('Hello world!');
+console.log('kdjflsjdfa');
+var t = 5;
+if (true) {
+    var t_1 = 1;
+    console.log(t_1);
+}
+console.log(t);
+console.log("this is a " + t);
--- new.ts
+++ new.ts
@@ -1,2 +1,11 @@
 console.log(1+3);
 console.log('Hello world!');
+console.log('kdjflsjdfa');
+
+let t=5;
+if(true){
+    let t=1;
+    console.log(t);
+}
+console.log(t);
+console.log(`this is a ${t}`);
